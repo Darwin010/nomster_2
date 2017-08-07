f56ef2f85a3beb7f528986a73005550fd2bcab9a
Walter Lopez <darwin010cr2@gmail.com>
2017-08-07 11:49:31 -0700

    place details

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,27 @@
 class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
-
+	
 	def index
 		@places = Place.all.page params[:page]
 	end
-
+	
 	def new
 		@place = Place.new
 	end
-
+	
 	def create
-		current_user.places.create place_params
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
-
-	private
-
-	def place_params
-		params.require(:place).permit :name, :description, :address
+	
+	def show
+		@place = Place.find(params[:id])
 	end
-
+	
+	private
+	
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
+	
 end
