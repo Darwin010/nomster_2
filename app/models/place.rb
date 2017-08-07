e0e4ab05c297d7fb301af614f7e5d33fef12d36f
Walter Lopez <darwin010cr2@gmail.com>
2017-08-07 14:40:06 -0700

    validation pt.2

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, length: { minimum: 3 , too_short: "is too short (minimum is %{count} characters)"}
+  validates :address, :description, presence: true
   paginates_per 3
 end
