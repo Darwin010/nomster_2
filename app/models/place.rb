063873336c41be8288c02c705e6c43ec06b7b549
Walter Lopez <darwin010cr2@gmail.com>
2017-08-08 15:46:43 -0700

    comments in db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :name, length: { minimum: 3 , too_short: "is too short (minimum is %{count} characters)"}
   validates :address, :description, presence: true
   geocoded_by :address
